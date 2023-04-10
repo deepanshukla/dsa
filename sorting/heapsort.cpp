d19a327007041a5da978bc97949515c397d547df
deepanshukla <126414692+deepanshukla@users.noreply.github.com>
2023-04-10 18:31:11 +0530

    Create heapsort.cpp

---
+++ sorting/heapsort.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+void heapify(int arr[], int n, int i)  
+{
+    int largest = i ;
+    int left = 2*i+1;
+    int right = 2*i+2;
+    if(left<n && arr[largest]<arr[left]){
+        largest = left;
+    }
+    if(right<n && arr[largest]<arr[right]){
+        largest = right;
+    }
+    if(largest!=i){
+        swap(arr[i],arr[largest]);
+        heapify(arr,n,largest);
+    }       
+}
+
+void buildHeap(int arr[], int n)  
+{ 
+    for(int i = (n/2)-1; i>=0;i--){
+        heapify(arr,n,i);
+    }   
+}
+void heapSort(int arr[], int n)
+{
+    buildHeap(arr,n);
+    for(int i =n-1;i>=0;i--){
+        swap(arr[0],arr[i]);
+        heapify(arr,i,0);
+    }
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+    heapSort(arr,n-1);
+    for (auto &&i : arr)
+    {
+        cout<<i<<" ";
+    }
+}
+
